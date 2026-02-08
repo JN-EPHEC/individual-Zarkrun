5332dc31e8a5134d4a5b582e474eb85d9a4c3036
Gaylor <gaylor.lamand@gmail.com>
2026-02-08 22:29:18 +0100

    Utilisation de express.router

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import userRoutes from "./routes/userRoutes.ts"
 
 const app = express();
-const port = 3000;µ
+const port = 3000;
 
 const etudiants = [
     { id: 1, nom: "Dupont", prenom: "Jean" },
@@ -17,6 +18,9 @@ app.get('/api/hello/:name', (request, response) => {
     const name = request.params.name
     response.json({ "message": `Bonjour ${name}`, "timestamp": new Date().toISOString() });
 });
+
+app.use('/api/users', userRoutes);
+
 app.listen(port, () => {
     //console.log(etudiants);
 })
