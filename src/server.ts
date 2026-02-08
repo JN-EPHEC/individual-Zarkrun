9ac4b08c1ada572b494b5eb7ec89aa5f8ad3b703
Gaylor <gaylor.lamand@gmail.com>
2026-02-08 21:44:30 +0100

    Utilisation d'une route pour retourner un tableau d'objets JSON

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,16 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+const etudiants = [
+    { id: 1, nom: "Dupont", prenom: "Jean" },
+    { id: 2, nom: "Martin", prenom: "Sophie" },
+    { id: 3, nom: "Doe", prenom: "John" },
+];
 
-app.get('/', (request, response) => {
-    response.send('Bienvenue sur mon serveur API');
+app.get('/api/data', (request, response) => {
+    response.json(etudiants);
 });
 
 app.listen(port, () => {
-    console.log(`Serveur lancé sur http://localhost:${port} `);
+    console.log(etudiants);
 })
