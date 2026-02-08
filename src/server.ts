57a12ce8adc999534d0afc4b7c3cbdbf48122aa5
Gaylor <gaylor.lamand@gmail.com>
2026-02-08 22:03:43 +0100

    Utilisation de paramètre dynamique

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 3000;µ
+
 const etudiants = [
     { id: 1, nom: "Dupont", prenom: "Jean" },
     { id: 2, nom: "Martin", prenom: "Sophie" },
@@ -12,6 +13,10 @@ app.get('/api/data', (request, response) => {
     response.json(etudiants);
 });
 
+app.get('/api/hello/:name', (request, response) => {
+    const name = request.params.name
+    response.json({ "message": `Bonjour ${name}`, "timestamp": new Date().toISOString() });
+});
 app.listen(port, () => {
-    console.log(etudiants);
+    //console.log(etudiants);
 })
