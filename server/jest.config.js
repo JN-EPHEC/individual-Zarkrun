794cd5ba5e7a3caeed8c89240fc737de96a423df
Gaylor <gaylor.lamand@gmail.com>
2026-04-15 17:38:26 +0200

    Rechangement jest

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,18 +1,22 @@
+import { createDefaultPreset } from "ts-jest";
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
 export default {
-  preset: "ts-jest",
   testEnvironment: "node",
 
   transform: {
-    "^.+\\.ts$": "ts-jest",
+    ...tsJestTransformCfg,
   },
 
   testPathIgnorePatterns: ["/dist/"],
 
-  moduleFileExtensions: ["ts", "js", "json"],
-
-  roots: ["<rootDir>/src"],
-
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
+
+  moduleFileExtensions: ["ts", "js", "json"],
+
+  roots: ["<rootDir>/src"],
 };
