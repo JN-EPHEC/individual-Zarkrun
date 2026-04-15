2090e2ca95392e9b42853113625c9d1aa3fc1938
Gaylor <gaylor.lamand@gmail.com>
2026-04-15 17:28:19 +0200

    Changement github workflow + handling des imports

--- server/jest.config.js
+++ server/jest.config.js
@@ -5,7 +5,18 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 export default {
   testEnvironment: "node",
+
   transform: {
     ...tsJestTransformCfg,
   },
+
+  testPathIgnorePatterns: ["/dist/"],
+
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+
+  moduleFileExtensions: ["ts", "js", "json"],
+
+  roots: ["<rootDir>/src"],
 };
